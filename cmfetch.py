4021247ba22388e4957e2d216f502a40e058dab2
Çınar Yılmaz <79412062+Camroku@users.noreply.github.com>
2021-08-29 05:30:11 +0300

    Add files via upload

---
+++ cmfetch.py
@@ -0,0 +1,13 @@
+import re
+
+computer_name = open("/sys/devices/virtual/dmi/id/product_name").read().replace("\n", "")
+cpu_name = open("/proc/cpuinfo").read().splitlines()[4].split(": ")[1]
+distro = re.search("PRETTY_NAME=\"(.*?)\"", open("/etc/os-release").read()).group(1)
+ram_total = str(round(int(re.search("MemTotal:        (.*?) kB", open("/proc/meminfo").read()).group(1)) / 1024))
+ram_used = str(round(int(re.search("MemAvailable:    (.*?) kB", open("/proc/meminfo").read()).group(1)) / 1024))
+
+print("\x1b[32mComputer Name: \x1b[34m" + computer_name)
+print("\x1b[32mCPU Name: \x1b[34m" + cpu_name)
+print("\x1b[32mDistro: \x1b[34m" + distro)
+print("\x1b[32mRam: \x1b[34m" + ram_used + "/" + ram_total + " MB")
+print("\x1b[0m", end="")
